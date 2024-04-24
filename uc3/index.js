ac171cc00ed50ec8d42c6faec3713619213eb8ad
Deeksha <93386786+DEE-KRISH@users.noreply.github.com>
2024-04-24 13:06:31 +0530

    Update index.js

--- uc3/index.js
+++ uc3/index.js
@@ -95,7 +95,7 @@ app.get('/history/:user',async(req,res)=>{
     const uresult = await userhistory.findOne({user:user})
     res.status(200).json(uresult.cart_history)
 })
-mongoose.connect(process.env.MURL) //connect to database , then create middleware server
+mongoose.connect("mongodb+srv://deekshakrish488:Wo2D19diCdgN8Jaf@cluster0.29qh5gt.mongodb.net/") //connect to database , then create middleware server
 .then(()=>{
     app.listen(5003,()=>console.log("uc3 running"))
 }).catch((error) => {
